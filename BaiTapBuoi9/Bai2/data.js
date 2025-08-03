56b13157eb5e9ac4b73d4603d0e7385e189879eb
DaoMynhDuc <167088325+DaoMynhDuc@users.noreply.github.com>
2025-08-03 12:27:29 +0700

    commit bai tap buoi 9

---
+++ BaiTapBuoi9/Bai2/data.js
@@ -0,0 +1,38 @@
+const transactions = [
+    {
+      id: 1001,
+      khachHang: "Nguyễn Văn An",
+      nhanVien: "Mai Thị Hoa",
+      soTien: 320000,
+      ngayMua: "30/07/2025"
+    },
+    {
+      id: 1002,
+      khachHang: "Trần Thị Bình",
+      nhanVien: "Nguyễn Văn Hùng",
+      soTien: 150000,
+      ngayMua: "29/07/2025"
+    },
+    {
+      id: 1003,
+      khachHang: "Lê Hữu Cường",
+      nhanVien: "Phạm Thị Hạnh",
+      soTien: 480000,
+      ngayMua: "28/07/2025"
+    },
+    {
+      id: 1004,
+      khachHang: "Phạm Minh Đức",
+      nhanVien: "Nguyễn Thị Thu",
+      soTien: 210000,
+      ngayMua: "27/07/2025"
+    },
+    {
+      id: 1005,
+      khachHang: "Vũ Thị Hương",
+      nhanVien: "Trần Văn Quang",
+      soTien: 370000,
+      ngayMua: "26/07/2025"
+    }
+  ];
+  
